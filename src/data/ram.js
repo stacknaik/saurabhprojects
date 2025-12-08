6179ad57e953a8c496d5ec4175f28218618e9155
stacknaik <saurabhcode106@gmail.com>
2025-12-08 11:40:14 +0530

    Create ram.js

---
+++ src/data/ram.js
@@ -0,0 +1,14 @@
+export const ram = [
+  { id: 1, src: "/images/ram1.jpg", pname: "vengenanace lpx dd3r5", info: "Best for basic tasks." },
+  { id: 2, src: "/images/ram2.jpg", pname: "Intel Core i9", info: "Top-tier performance." },
+  { id: 3, src: "/images/ram3.jpg", pname: "Ryzen 9 5900X", info: "Flagship performance." },
+  { id: 4, src: "/images/ram4.jpg", pname: "Rip Jaws R3", info: "Best for basic tasks." },
+  { id: 5, src: "/images/ram5.jpg", pname: "Kingster Fury", info: "Best for basic tasks." },
+  { id: 6, src: "/images/ram6.jpg", pname: "Trident JS", info: "Best for basic tasks." },
+  { id: 7, src: "/images/ram7.jpg", pname: "Trident Z RGB", info: "Best for basic tasks." },
+  { id: 8, src: "/images/ram8.jpg", pname: "RipJaws S5", info: "Best for basic tasks." },
+  { id: 9, src: "/images/ram9.jpg", pname: "RipJaws V15", info: "Best for basic tasks." },
+  { id: 10, src: "/images/ra10.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 11, src: "/images/ram11.jpg", pname: "ASkill AEGIS DDR4", info: "Best for basic tasks." },
+  { id: 12, src: "/images/ram12.jpg", pname: "Kingsten Fury  S3", info: "Best for basic tasks." },
+];
