9e2d69700114cea5a1f5f5820e87de357892950b
stacknaik <saurabhcode106@gmail.com>
2025-12-09 20:04:01 +0530

    Add new file

---
+++ src/data/cables.js
@@ -0,0 +1,15 @@
+export const graphics = [
+  { id: 1, src: "/images/c1.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 2, src: "/images/c2.jpg", pname: "Intel Core i9", info: "Top-tier performance." },
+  { id: 3, src: "/images/c3.jpg", pname: "Ryzen 9 5900X", info: "Flagship performance." },
+  { id: 4, src: "/images/c4.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 5, src: "/images/c5.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 6, src: "/images/c6.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 7, src: "/images/c7.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 8, src: "/images/c8.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 9, src: "/images/c9.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 10, src: "/images/c10.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 11, src: "/images/c11.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 12, src: "/images/c12.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+];
+
---
+++ src/data/powersupply.js
@@ -0,0 +1,15 @@
+export const graphics = [
+  { id: 1, src: "/images/ps1.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 2, src: "/images/ps2.jpg", pname: "Intel Core i9", info: "Top-tier performance." },
+  { id: 3, src: "/images/ps3.jpg", pname: "Ryzen 9 5900X", info: "Flagship performance." },
+  { id: 4, src: "/images/ps4.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 5, src: "/images/ps5.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 6, src: "/images/ps6.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 7, src: "/images/ps7.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 8, src: "/images/ps8.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 9, src: "/images/ps9.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 10, src: "/images/ps10.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 11, src: "/images/ps11.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 12, src: "/images/ps12.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+];
+
