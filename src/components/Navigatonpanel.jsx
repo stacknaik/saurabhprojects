1ca1775ddc267fc3a3b2af6db52d3c6388a00855
stacknaik <saurabhcode106@gmail.com>
2025-12-09 22:54:11 +0530

    Add new file

--- src/components/Navigatonpanel.jsx
+++ src/components/Navigatonpanel.jsx
@@ -31,46 +31,40 @@ function Navigatonpanel() {
                 Processor
               </Link>
               <Link to="/graphics" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/graphics.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/graphics.svg" alt="Graphics" className="w-5 h-5 mr-2" />
+                Graphics
               </Link>
               <Link to="/ssd" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/ssd.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
-              </Link>
-    
-              <Link to="/processor" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/processor.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/ssd.svg" alt="SSD" className="w-5 h-5 mr-2" />
+                SSD
               </Link>
               <Link to="/ram" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/ram.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/ram.svg" alt="RAM" className="w-5 h-5 mr-2" />
+                RAM
               </Link>
-
               <Link to="/casefan" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/casefan.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/casefan.svg" alt="Case Fan" className="w-5 h-5 mr-2" />
+                Case Fan
               </Link>
               <Link to="/cables" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/cables.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/cables.svg" alt="Cables" className="w-5 h-5 mr-2" />
+                Cables
               </Link>
               <Link to="/powersupply" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/powersupply.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/powersupply.svg" alt="Power Supply" className="w-5 h-5 mr-2" />
+                Power Supply
           </Link>
               <Link to="/harddrives" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/harddrives.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/harddrives.svg" alt="Hard Drives" className="w-5 h-5 mr-2" />
+                Hard Drives
               </Link>
               <Link to="/usb" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/usb.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/usb.svg" alt="USB" className="w-5 h-5 mr-2" />
+                USB
               </Link>
               <Link to="/keyboard" className="px-4 py-2 dropdown-neon flex items-center">
-                <img src="/keyboard.svg" alt="Processor" className="w-5 h-5 mr-2" />
-                Processor
+                <img src="/keyboard.svg" alt="Keyboard" className="w-5 h-5 mr-2" />
+                Keyboard
               </Link>
 
             
