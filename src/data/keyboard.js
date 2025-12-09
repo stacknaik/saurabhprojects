0c773f081d893f4553f20fb9395a0a4c489a3929
stacknaik <saurabhcode106@gmail.com>
2025-12-09 20:11:16 +0530

    Add new file

---
+++ src/data/keyboard.js
@@ -0,0 +1,15 @@
+export const keyboard = [
+  { id: 1, src: "/images/k1.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 2, src: "/images/k2.jpg", pname: "Intel Core i9", info: "Top-tier performance." },
+  { id: 3, src: "/images/k3.jpg", pname: "Ryzen 9 5900X", info: "Flagship performance." },
+  { id: 4, src: "/images/k4.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 5, src: "/images/k5.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 6, src: "/images/k6.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 7, src: "/images/k7.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 8, src: "/images/k8.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 9, src: "/images/k9.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 10, src: "/images/k10.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 11, src: "/images/k11.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 12, src: "/images/k12.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+];
+
