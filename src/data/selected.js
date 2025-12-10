a1e014c6843498fe402cb4ec2697dd6e54ca4c16
stacknaik <saurabhcode106@gmail.com>
2025-12-10 18:14:07 +0530

    Add new file

--- src/data/selected.js
+++ src/data/selected.js
@@ -1,5 +1,15 @@
+import { harddrives } from "./harddrives";
+
 export var selectedComponents = {
-  processor: null,
-  graphics: null,
-  ssd: null,
+    processor: null,
+    graphics: null,
+    ssd: null,
+    ram: null,
+    casefan: null,
+    cables: null,
+    powersupply: null,
+    harddrives: null,
+    usb: null,
+    keyboard: null
+
 };
